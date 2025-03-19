66583fcefb3d47ce870531d07f0a267d6a73f01b
Kaly <sandra.heraud@ynov.com>
2025-03-19 11:52:53 +0100

    Deck working

--- apps/frontend/src/components/game/Deck.tsx
+++ apps/frontend/src/components/game/Deck.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
+import { useGame } from "@/context/game/game-provider";
+
 export default function Deck(): React.JSX.Element {
-  
+  const { deckLength } = useGame();
   return (
-    <div className="flex flex-col m-2">
-      <div className="w-12 h-20 border border-gray-300 rounded-sm shadow-md bg-white relative">
+    <div className="flex flex-col m-2 justify-center items-center">
+      <div className="w-12 h-20 border border-gray-300 rounded-sm shadow-md bg-white relative ">
         <img
           src="/images/cards/back.png"
           className="w-full h-full rounded-sm"
         />
-        <p>nombre de cartes</p>
       </div>
+      <p className="absolute text-white font-bold text-xl">{deckLength}</p>
     </div>
   );
 }
